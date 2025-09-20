86ece0560447e68254fb4cdf4e7c10349b9fcafb
Ma-Singo <codesingo@gmail.com>
2025-09-20 08:43:07 +0100

    Fixing default login and logout fxnality and adding a profile page to check user info

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignupView
+from .views import SignupView, profile_view
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
+    path('profile', profile_view, name='profile')
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 from django.views.generic import CreateView
 
 from .forms import CustomUserCreationForm
@@ -8,3 +9,7 @@ class SignupView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+@login_required
+def profile_view(request):
+    return render(request, 'registration/profile.html')
